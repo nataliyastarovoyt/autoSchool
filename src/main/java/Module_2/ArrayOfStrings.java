5e35ec733f02278a66e65752b34392662ecf4a15
nataliyastarovoyt <nataliya.starovoyt@nixsolutions.com>
2022-06-01 11:13:24 +0200

    Add tasks 1-4 in ArrayOfString class

--- src/main/java/Module_2/ArrayOfStrings.java
+++ src/main/java/Module_2/ArrayOfStrings.java
@@ -3,12 +3,14 @@ package Module_2;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class ArrayOfStrings {
     public static void main(String[] args) throws Exception {
         task1();
         task2();
-
+        task3();
+        task4();
 
     }
 
@@ -41,5 +43,42 @@ public class ArrayOfStrings {
             System.out.println(numbers[i]);
         }
     }
+
+    public static void task3() throws IOException {
+        int[] mass = new int[10];
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        for (int i = 0; i < mass.length; i++) {
+            mass[i] = Integer.parseInt(reader.readLine());
+        }
+        for (int i = 0; i < 5; i++) {
+            int t = mass[i];
+            mass[i] = mass[mass.length - 1 - i];
+            mass[mass.length - 1 - i] = t;
+        }
+        for (int i = 0; i < mass.length; i++) {
+            System.out.println(mass[i]);
+        }
+    }
+
+    public static void task4() throws IOException {
+        int[] bigArray = new int[20];
+        int[] smallArray1 = new int[10];
+        int[] smallArray2 = new int[10];
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int i = 0; i < bigArray.length; i++) {
+            bigArray[i] = Integer.parseInt(reader.readLine());
+        }
+
+        smallArray1 = Arrays.copyOfRange(bigArray, 0, 9);
+        smallArray2 = Arrays.copyOfRange(bigArray, 10, 20);
+
+        for (int i = 0; i < smallArray2.length; i++) {
+            System.out.println(smallArray2[i]);
+        }
+    }
 }
+
+
 
